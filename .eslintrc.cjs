94e762d4a627498fa980f1e2f6e72de30764b725
VNagorniy <vnagorniyrnd@gmail.com>
2024-05-15 19:27:11 +0300

    Компоненты (что такое JSX? React Fragment, создание и стилизация компонента, props, композиция(children))

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,5 +12,7 @@ module.exports = {
     semi: ['error', 'always', { omitLastInOneLineBlock: false }],
     'comma-dangle': ['error', 'never'],
     quotes: ['error', 'single'],
-  },
+    'react/prop-types': [0],
+    indent: ['error', 'tab']
+  }
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import './App.css';
+import CardButton from './CardButton/CardButton';
+import JournalItem from './JournalItem/JournalItem';
+import Button from './components/Button/Button';
 
 function App() {
-  return <div>Проект</div>;
+	const data = [
+		{
+			title: 'Подготовка к прохождению курсов',
+			text: 'Инициализация проекта',
+			date: new Date()
+		},
+		{ title: 'Разбор props', text: 'Прокидывание пропсов', date: new Date() }
+	];
+
+	return (
+		<>
+			<h1>Заголовок</h1>
+			<p>Какой-то текст</p>
+			<Button />
+			<CardButton>Новое воспоминание</CardButton>
+			<CardButton>
+				<JournalItem title={data[0].title} text={data[0].text} date={data[0].date} />
+			</CardButton>
+			<JournalItem title={data[1].title} text={data[1].text} date={data[1].date} />
+		</>
+	);
 }
 
 export default App;
