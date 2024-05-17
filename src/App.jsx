971468e00b4f321fd224f0dd275669fa58549745
VNagorniy <vnagorniyrnd@gmail.com>
2024-05-17 15:24:52 +0300

    Блок 2.Отображение данных. Использование key в списках

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,12 @@ import { useState } from 'react';
 
 const INITIAL_DATA = [
 	{
+		id: 1,
 		title: 'Подготовка к прохождению курсов',
 		text: 'Инициализация проекта',
 		date: new Date()
 	},
-	{ title: 'Разбор props', text: 'Прокидывание пропсов', date: new Date() }
+	{ id: 2, title: 'Разбор props', text: 'Прокидывание пропсов', date: new Date() }
 ];
 
 function App() {
@@ -27,9 +28,18 @@ function App() {
 			{
 				text: item.text,
 				title: item.title,
-				date: new Date(item.date)
+				date: new Date(item.date),
+				id: Math.max(...oldItems.map((i) => i.id)) + 1
 			}
 		]);
+	};
+
+	const sortItems = (a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		} else {
+			return -1;
+		}
 	};
 
 	return (
@@ -39,8 +49,8 @@ function App() {
 					<Header />
 					<JournalAddButton />
 					<JournalList>
-						{items.map((el) => (
-							<CardButton>
+						{items.sort(sortItems).map((el) => (
+							<CardButton key={el.id}>
 								<JournalItem title={el.title} text={el.text} date={el.date} />
 							</CardButton>
 						))}
