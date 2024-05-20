46d7517397c05bb2d20b190e757700843d3d1fc4
VNagorniy <vnagorniyrnd@gmail.com>
2024-05-20 17:53:23 +0300

    Блок 3.Работа React. Просмотр работы Batching

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import Button from '../Button/Button';
 import SelectUser from '../SelectUser/SelectUser';
 import Logo from '../Logo/Logo';
@@ -6,10 +6,13 @@ import Logo from '../Logo/Logo';
 const logos = ['/logo.svg', './folder.svg'];
 function Header() {
 	const [logoIndex, setLogoIndex] = useState(0);
+	const [secondIndex, setSecondIndex] = useState(0);
 
-	const toggleIndex = useCallback(() => {
+	const toggleIndex = () => {
 		setLogoIndex((state) => Number(!state));
-	}, []);
+		setSecondIndex((i) => i + 1);
+		console.log(secondIndex);
+	};
 	return (
 		<>
 			<Logo image={logos[logoIndex]} />
