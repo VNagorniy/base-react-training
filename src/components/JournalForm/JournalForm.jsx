b107d33677dc892b96e1c5b8e9701b3c1f786e09
VNagorniy <vnagorniyrnd@gmail.com>
2024-05-20 15:23:06 +0300

    Блок 3.Контекст. Реализация контекста на примере 2х юзеров

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -48,6 +48,10 @@ function JournalForm({ onSubmit }) {
 		}
 	}, [isFormReadyToSubmit, values, onSubmit]);
 
+	useEffect(() => {
+		dispatchForm({ type: 'SET_VALUE', payload: { userId } });
+	}, [userId]);
+
 	const onChange = (e) => {
 		dispatchForm({ type: 'SET_VALUE', payload: { [e.target.name]: e.target.value } });
 	};
@@ -58,7 +62,6 @@ function JournalForm({ onSubmit }) {
 	};
 	return (
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
-			{userId}
 			<div>
 				<Input type="text" ref={titleRef} isValid={isValid.title} onChange={onChange} value={values.title} name="title" appearance="title" />
 			</div>
