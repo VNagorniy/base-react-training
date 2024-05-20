3b63995fe33ce797e7bd479038dc6b159bc0c154
VNagorniy <vnagorniyrnd@gmail.com>
2024-05-20 18:06:45 +0300

    Блок 3.Работа React. Использование useMemo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,9 @@ import Logo from '../Logo/Logo';
 const logos = ['/logo.svg', './folder.svg'];
 function Header() {
 	const [logoIndex, setLogoIndex] = useState(0);
-	const [secondIndex, setSecondIndex] = useState(0);
 
 	const toggleIndex = () => {
 		setLogoIndex((state) => Number(!state));
-		setSecondIndex((i) => i + 1);
-		console.log(secondIndex);
 	};
 	return (
 		<>
--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import CardButton from '../CardButton/CardButton';
 import JournalItem from '../JournalItem/JournalItem';
 import './JournalList.css';
@@ -7,10 +7,6 @@ import { UserContext } from '../../context/user.context';
 function JournalList({ items }) {
 	const { userId } = useContext(UserContext);
 
-	if (items.length === 0) {
-		return <p>Записей пока нет, добавьте первую.</p>;
-	}
-
 	const sortItems = (a, b) => {
 		if (a.date < b.date) {
 			return 1;
@@ -18,17 +14,19 @@ function JournalList({ items }) {
 			return -1;
 		}
 	};
+	const filteredItems = useMemo(() => items.filter((el) => el.userId === userId).sort(sortItems), [items, userId]);
+
+	if (items.length === 0) {
+		return <p>Записей пока нет, добавьте первую.</p>;
+	}
 
 	return (
 		<>
-			{items
-				.filter((el) => el.userId === userId)
-				.sort(sortItems)
-				.map((el) => (
-					<CardButton key={el.id}>
-						<JournalItem title={el.title} post={el.post} date={el.date} />
-					</CardButton>
-				))}
+			{filteredItems.map((el) => (
+				<CardButton key={el.id}>
+					<JournalItem title={el.title} post={el.post} date={el.date} />
+				</CardButton>
+			))}
 		</>
 	);
 }
